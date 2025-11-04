873d0503768b80c2275f52e6bd8052f67574317a
Dimas <dimas436@bk.ru>
2025-11-04 19:02:41 +0300

    ended catalog + js для catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,58 @@
+// карусель
 $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
+        responsive: [ // настройка карусели под маленький экран
             {
                 breakpoint: 992,
-                settings: {
+                settings: {                    
                     dots: true,
                     arrows: false
                 }
             }
         ]
     });
+// включение вкладок для фитнеса/для бега/для триатлона
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+// включение кнопки "ПОДРОБНЕЕ" 1 вариант:
+
+    // $('.catalog-item__link').each(function(i) {
+    //     $(this).on('click', function(e){
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    // });
+
+// включение кнопки "НАЗАД" 1 вариант:
+
+    // $('.catalog-item__back').each(function(i) {
+    //     $(this).on('click', function(e){
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    // });
+
+
+// включение кнопок "ПОДРОБНЕЕ/НАЗАД" 2 вариант:
+    function toggleSlide(item) {
+       $(item).each(function(i) {
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });  
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
